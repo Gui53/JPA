17881f08858b52fbaa60ddc9c5d0080d6c853561
Gui53 <gui53tube@gmail.com>
2023-12-08 12:08:54 -0300

    Atividade professor

---
+++ aula/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into tb_contatos(id,nome,email,fone)values(1,'Maria','maria@gmail.com','489965432212')
+insert into tb_contatos(id,nome,email,fone)values(2,'Gui','gui@gmail.com','489965432212')
+
+insert into tb_local(id,nome,rua,num,bairro,cidade,uf,cep)values(1, 'Campo do João','Estrada Geral RSJ',00,'Barra Clara','Angelina','SC','88460-000')
